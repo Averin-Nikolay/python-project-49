48728e1362afa9ec84011adb0ade1dca65d65307
Averin-Nikolay <Averin-Nikolay@users.noreply.github.com>
2022-11-22 14:31:56 +0400

    fix

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from brain_games.cli import welcome_user
+
+print('brain-games') # в задании есть но не уверен
+
+def main():
+	print('Welcome to the Brain Games!')
+	print(welcome_user())
+
+
+if __name__ == '__main__':
+	main()
+
+# welcome_user()
